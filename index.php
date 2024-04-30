1c0a28723973353441f44a0b5bbffc6a4bdff7f1
Engels Grau <jevgm.03@gmail.com>
2024-04-29 20:32:28 -0400

    Subiendo las carpetas bootstrap y haciendo el archivo index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$pagina = 'login';
+
+if(!empty ($_GET['pagina'])){
+    $pagina = $_GET['pagina']; 
+}
+
+if(is_file('controlador/'.$pagina.".php")){
+    require_once  "controlador/".$pagina.".php";   //si la pagina existe, cargamos el archivo correspondiente
+}else {                                          //sino, mostramos un error de que no se encuentra la pagina solicitada
+    echo 'PÁGINA EN CONSTRUCCIÓN';
+}
