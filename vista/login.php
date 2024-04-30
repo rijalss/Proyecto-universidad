475f09a57a7f697bee8d84021f744b788b5a0d65
Liiamson <104730086+Liiamson@users.noreply.github.com>
2024-04-29 23:47:29 -0400

    Mensaje de commit

--- vista/login.php
+++ vista/login.php
@@ -9,8 +9,18 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-
-<section class="p-3 p-md-4 p-xl-5">
+<div class="container">
+  <div class="row justify-content-center mt-5">
+    <div class="col-12 col-md-8 col-lg-6">
+      <div class="card border-light-subtle shadow-sm">
+        Orlando pendejo
+      </div>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col">
+      <section class="p-4 p-md-12 p-xl-14">
+      <section class="p-4 p-md-12 p-xl-14">
   <div class="container">
     <div class="card border-light-subtle shadow-sm">
       <div class="row g-0">
@@ -52,6 +62,12 @@
   </div>
   
 </section>
+
+      </section>
+    </div>
+  </div>
+</div>
+
   
 
 <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
