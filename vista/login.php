cf51ab3eb38139bf93b9aff9b6e84c9d3d6c9c4f
Engels Grau <jevgm.03@gmail.com>
2024-04-29 21:07:39 -0400

    estructura del proyecto

---
+++ vista/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+
+
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
