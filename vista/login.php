db2eb035d978de6c7df6f108fb3ab80a61ad89e8
Liiamson <104730086+Liiamson@users.noreply.github.com>
2024-04-29 23:08:02 -0400

    Mensaje de commit

--- vista/login.php
+++ vista/login.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,31 +15,31 @@
     <div class="card border-light-subtle shadow-sm">
       <div class="row g-0">
         <div class="col-12 col-md-6">
-          <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="./assets/img/logo-img-1.webp" alt="BootstrapBrain Logo">
+          <img class="img rounded-start w-200 h-200 object-fit-cover" loading="lazy" src="img/img-1.jpg" alt="BootstrapBrain Logo">
         </div>
         <div class="col-12 col-md-6">
           <div class="card-body p-3 p-md-4 p-xl-5">
             <div class="row">
               <div class="col-12">
                 <div class="mb-5">
-                  <h3>Log in</h3>
+                  <h3>Inciar Sesión</h3>
                 </div>
               </div>
             </div>
             <form action="#!">
               <div class="row gy-3 gy-md-4 overflow-hidden">
                 <div class="col-12">
-                  <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+                  <label for="email" class="form-label">Correo <span class="text-danger">*</span></label>
                   <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
                 </div>
                 <div class="col-12">
-                  <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+                  <label for="password" class="form-label">Contraseña <span class="text-danger">*</span></label>
                   <input type="password" class="form-control" name="password" id="password" value="" required>
                 </div>
                 
                 <div class="col-12">
                   <div class="d-grid">
-                    <button class="btn bsb-btn-xl btn-primary" type="submit">Log in now</button>
+                    <button class="btn bsb-btn-xl btn-primary" type="submit">Inicia ahora</button>
                   </div>
                 </div>
               </div>
@@ -56,4 +55,4 @@
 <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
->>>>>>> f96e1579c6e5ac464773be23295e456f58945fcd
+
