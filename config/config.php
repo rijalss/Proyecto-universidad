434fd869e3de691799fa1b98439680c2a4638e28
orlandoabarrientos <orlandoabarrientos@gmail.com>
2024-05-05 23:39:02 -0400

    otra vez configurando la base de datos :(

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //--------------------Base de Datos---------------------------
-define('_DB_NAME_', 'bdlibreria'); // nombre de la BD a conectar
+define('_DB_NAME_', 'sistemainventario'); // nombre de la BD a conectar
 define('_DB_HOST_', 'localhost'); //  servidor local
 define('_DB_USER_', 'root'); // usuario por defecto de MYSQL
 define('_DB_PASS_', ''); // password creada a la hora de instalar MYSQL  en mi compu la instale sin clase por esa razón esta vacia 
