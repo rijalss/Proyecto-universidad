030301221eca7bb66733a28ea553c570b65b6718
rijals <rijalscarra55@gmail.com>
2024-05-06 04:38:32 +0200

    agregue la carpeta config y database, y el archivo conexion en modelo

---
+++ modelo/conexion.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config/config.php';
+
+class Conexion extends PDO
+{
+
+    private $conex;
+
+    public function __construct()
+    {
+
+        $conexstring = "mysql:host=" . _DB_HOST_ . ";dbname=" . _DB_NAME_ . ";charset=utf8";
+
+        try {
+
+            $this->conex = new PDO($conexstring, _DB_USER_, _DB_PASS_);
+            $this->conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+
+            die("Conexión Fallida" . $e->getMessage());
+        }
+    }
+
+    protected function Conex()
+    {
+        return  $this->conex;
+    }
+}
