136b4ee516be4d5784887bfc16d8cbb013d3f0a7
Engels Grau <jevgm.03@gmail.com>
2024-05-09 22:39:32 -0400

    Personalizando página principal con helianta

--- public/commun/footer.php
+++ public/commun/footer.php
@@ -1,10 +1,14 @@
 <!-- TODO conectar a la principal -->
-<footer id="footer" class=" pt-4 pb-2">
-    <div class="container">
-        <div class="row">
-        <div class="col-md-12">
-            <p>&copy; 2024 Proyecto UPTAEB.</p>
+    <link rel="stylesheet" href="style.css">
+<body>
+    <footer id="footer" class="bg-dark text-white py-5">
+        <div class="container">
+            <div class="row">
+        <div class="col-md-4">
+            <div class="row mt-1 text-center">
+                <p>&copy; 2024 Todos los derechos reservados.</p>
+            </div>
         </div>
-        </div>
-    </div>
-</footer>
+    </footer>
+</body>
+</html>
--- views/principal.php
+++ views/principal.php
@@ -6,14 +6,90 @@
     <title>Bienvenido</title>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/bootstrap/css/sidebar.css">
+    <link rel="stylesheet" href="public/bootstrap/css/style.css">
 </head>
 <body>
     <!-- Header -->
         <?php require_once("public/commun/menu.php");?>
     <!-- Header -->
-	
-    
+
+    <!-- Title -->
+    <br><br><br><br>
+    <h1 class="display-4 text-center text-uppercase font-weight-bold">Bienvenido!</h1>
+    <br><br>
+    <!-- Container de cards -->
+    <div class="container d-flex justify-content-center">
+    <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Proveedores</h5>
+                <p class="card-text ">Gestiona tus proveedores con facilidad.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Almacen</h5>
+                <p class="card-text"> Gestiona tus almacenes de manera eficiente.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Productos</h5>
+                <p class="card-text">Mantén tu inventario bajo control</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Usuarios</h5>
+                <p class="card-text">Controla el acceso a tu sistema</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Ventas</h5>
+                <p class="card-text">Gestiona tus ventas con facilidad.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Clientes</h5>
+                <p class="card-text">Gestiona tus clientes con facilidad.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card shadow w-100 h-100 p-3">
+            <div class="card-body">
+                <h5 class="card-title">Cotizaciones</h5>
+                <p class="card-text">Gestiona tus cotizaciones con facilidad.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br>
+    <!-- Footer -->
+    <?php require_once("public/commun/footer.php");?>
+    <!-- Footer -->
+
     <!-- Scripts -->
         <script src="public/bootstrap/js/sidebar.js"></script>
         <script src="public/bootstrap/js/bootstrap.bundle.min.js"></script>
